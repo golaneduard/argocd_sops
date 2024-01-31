1ad1ee68216ca7f2c95cde541ada1bd81c01b111
Golan Eduard <golaneduard1@gmail.com>
2024-02-01 00:13:08 +0200

    Update for GCR

--- deployment/helmcharts/fastapi/decrypt_sops_secrets.sh
+++ deployment/helmcharts/fastapi/decrypt_sops_secrets.sh
@@ -1,34 +1,14 @@
 #!/bin/bash
 
-#secrets_path=secrets
-#config_path=config
-#temporary_name=secret.decrypted.json
-#decrypted_file_name=secret.service.account.key.decrypted.json
 secrets_path=secrets/$ENVIRONMENT
-ARN=arn:aws:kms:eu-central-1:209591221760:key/6c7d633f-e5b0-4c88-a72a-ee864caeb5f3
-
-# Loop for decrypt service account key and format like passowrd for docker pull secret "_helper.tpl"
-#decrypt_service_account_key () {
-#    for file in $secrets_path/*; do
-#        sops -d $file > $secrets_path/$decrypted_file_name.temporary
-#        jq -c . $secrets_path/$decrypted_file_name.temporary > $secrets_path/$decrypted_file_name.temporary2
-#        perl -pe 's/"/\\"/g' $secrets_path/$decrypted_file_name.temporary2 > $secrets_path/$decrypted_file_name.temporary3
-#        perl -pe 's/\\n/\\\\n/g' $secrets_path/$decrypted_file_name.temporary3 > $secrets_path/$decrypted_file_name.temporary4
-#        cat $secrets_path/$decrypted_file_name.temporary4 | tr -d "[:space:]" > $secrets_path/$decrypted_file_name
-#        rm -f $secrets_path/$decrypted_file_name.*
-#    done
-#}
+#ARN=arn:aws:kms:eu-central-1:209591221760:key/6c7d633f-e5b0-4c88-a72a-ee864caeb5f3
 
 # Loop for decrypt secrets in config folder
 decrypt_envfile_secrets () {
     for folder in $secrets_path; do
-#        if [[ -f $folder/secrets.yaml ]]
-#        then
-            sops -d --kms $ARN $folder/secrets.yaml > $folder/secrets.decrypted.yaml
-#            sops -d $folder/google-application-credentials.json > $folder/google-application-credentials.decrypted.json
-#        fi
+#            sops -d --kms $ARN $folder/secrets_backup.yaml > $folder/secrets.yaml
+            sops -d $folder/secrets.yaml > $folder/secrets.decrypted.yaml
     done
 }
 
-#decrypt_service_account_key
 decrypt_envfile_secrets
